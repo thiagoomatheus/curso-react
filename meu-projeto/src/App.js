dc9e7bfe80248eb98e5244e134ceca4d3cd321af
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-06-30 17:36:05 -0300

    aula 1 a 4

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import HelloWord from './components/HelloWord'
+
+function App() {
+  return (
+    <div className="App">
+    <HelloWord />
+  </div>
+  )
+
+
+  // const data = {
+  //   name: "Thiago",
+  //   work: "programador"
+  // }
+
+
+  // return (
+  //   <div className="App">
+  //     <h1>Testando o JSX</h1>
+  //     <p>{data.name} quer ser {data.work}</p>
+  //   </div>
+  // );
+}
+
+export default App;
