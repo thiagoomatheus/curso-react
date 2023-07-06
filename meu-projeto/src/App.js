bf6d008f473a1b1c2a2f8e111f64913babf0ffeb
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-07-06 15:34:27 -0300

    Aula 7-10

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
-import HelloWord from './components/HelloWord'
-import Pessoa from "./components/Pessoa"
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-    <HelloWord />
-    <Pessoa nome="Thiago" idade="22" profissao="programador" foto="https://via.placeholder.com/150" />
+  <div className="App">
+    <Form />
   </div>
   )
-
-
-  // const data = {
-  //   name: "Thiago",
-  //   work: "programador"
-  // }
-
-
-  // return (
-  //   <div className="App">
-  //     <h1>Testando o JSX</h1>
-  //     <p>{data.name} quer ser {data.work}</p>
-  //   </div>
-  // );
 }
 
 export default App;
